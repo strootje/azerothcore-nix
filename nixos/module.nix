bce105909c76d3bdba74478e6fee38e52eadf4b0
Bastiaan <bas@strootje.com>
2026-08-18 10:45:11 +0200

    feat: include managed mysql

--- nixos/module.nix
+++ nixos/module.nix
@@ -19,6 +19,47 @@ in
     };
   };
 
+  options.services.azerothcore.database = {
+    managed = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    host = lib.mkOption {
+      type = lib.types.str;
+      default = "localhost";
+    };
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3306;
+    };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "acore";
+    };
+
+    # passwordFile = lib.mkOption {
+    #   type = lib.types.path;
+    # };
+
+    authDatabase = lib.mkOption {
+      type = lib.types.str;
+      default = "acore_auth";
+    };
+
+    worldDatabase = lib.mkOption {
+      type = lib.types.str;
+      default = "acore_world";
+    };
+
+    characterDatabase = lib.mkOption {
+      type = lib.types.str;
+      default = "acore_characters";
+    };
+  };
+
   config = lib.mkIf cfg.enable {
     users.groups.acore = { };
     users.users.acore = {
@@ -29,6 +70,28 @@ in
 
     environment.etc."azerothcore/authserver.conf".source = "${cfg.package}/etc/authserver.conf.dist";
     environment.etc."azerothcore/worldserver.conf".source = "${cfg.package}/etc/worldserver.conf.dist";
+
+    services.mysql = lib.mkIf cfg.database.managed {
+      package = pkgs.mariadb;
+      enable = true;
+
+      ensureDatabases = [
+        cfg.database.authDatabase
+        cfg.database.worldDatabase
+        cfg.database.characterDatabase
+      ];
+
+      ensureUsers = [
+        {
+          name = cfg.database.user;
+          ensurePermissions = {
+            "${cfg.database.authDatabase}.*" = "ALL PRIVILEGES";
+            "${cfg.database.worldDatabase}.*" = "ALL PRIVILEGES";
+            "${cfg.database.characterDatabase}.*" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+    };
 
     systemd.services.auhtserver = {
       description = "AzerothCore AuthServer";
